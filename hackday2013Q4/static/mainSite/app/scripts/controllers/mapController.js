4fbaa60b03f6214a8ed94fd4183764388b43f561
fangyue <fangyuew@yahoo-inc.com>
2013-11-14 20:50:40 -0800

    geocoding

--- hackday2013Q4/static/mainSite/app/scripts/controllers/mapController.js
+++ hackday2013Q4/static/mainSite/app/scripts/controllers/mapController.js
@@ -2,13 +2,26 @@
         var module = angular.module("hackday", ["google-maps"]);
 }());
 
-function mapController ($scope, $timeout, $log) {
+function mapController ($scope, $timeout, $log)
+{
   
     // Enable the new Google Maps visuals until it gets enabled by default.
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
     google.maps.visualRefresh = true;
-        
-    angular.extend($scope,
+
+    $scope.NaviEntity=
+    {
+        from:{ address:'', latlng:''},
+        to:{ address:'', latlng:''}
+    }
+
+    $scope.centerProperty =
+            {
+                latitude: 37.4168811,
+                longitude: -122.02561550000001
+            };
+
+    angular.extend( $scope,
         {
             position:
             {
@@ -20,18 +33,14 @@ function mapController ($scope, $timeout, $log) {
             },
 
             /** the initial center of the map */
-            centerProperty:
-            {
-                latitude: 45,
-                longitude: -73
-            },
+
 
             /** the initial zoom level of the map */
-            zoomProperty: 4,
+            zoomProperty: 14,
 
             /** list of markers to put in the map */
             markersProperty:
-            [ {latitude: 45,longitude: -74}
+            [
             ],
 
             // These 2 properties will be set when clicking on the map
@@ -47,5 +56,35 @@ function mapController ($scope, $timeout, $log) {
                     $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
                   }
             }
-        });
+        }
+    );
+
+
+    $scope.search=function(ori, dest)
+    {
+        console.log("From : "+ori);
+        console.log("To : "+dest);
+
+        var geocoder = new google.maps.Geocoder();
+
+                console.log("search, do geocoding!");
+                address = '701 first ave, sunnyvale';
+                geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK)
+                {
+                    //map.setCenter(results[0].geometry.location);
+                    //var marker = new google.maps.Marker({
+                    //    map: map,
+                    //    position: results[0].geometry.location
+                    //});
+
+                    console.log("GeoCoding Result : "+results[0].geometry.location);
+                }
+                else
+                    {
+                        alert("Geocode was not successful for the following reason: " + status);
+                    }
+                });
+    }
+
 }
--- hackday2013Q4/static/mainSite/app/scripts/services/GeoLibrary.js
+++ hackday2013Q4/static/mainSite/app/scripts/services/GeoLibrary.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+var geocoder = new google.maps.Geocoder();
+
 angular.module('hackday')
   .factory('GeoLibrary', [ '$http', '$q', function ($http, $q)
   {
@@ -24,12 +26,28 @@ angular.module('hackday')
     functions.getGeoByAddress = function(address)
     {
         test();
+        address = '701 first ave, sunnyvale';
+        geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK)
+        {
+            //map.setCenter(results[0].geometry.location);
+            //var marker = new google.maps.Marker({
+            //    map: map,
+            //    position: results[0].geometry.location
+            //});
 
+            console.log("GeoCoding Result : "+results[0].geometry.location);
+        } else
+        {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
 
     };
 
     return functions;
   }]);
+
 
 function test()
 {
